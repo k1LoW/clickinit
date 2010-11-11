4462e3d8f13b1c0ac865651cba6890556b86d104
k1LoW <k1lowxb@gmail.com>
2010-11-11 19:07:35 +0900

    Modified clickinit.js

--- webroot/js/clickinit.js
+++ webroot/js/clickinit.js
@@ -1,12 +1,30 @@
 $(function() {
       $('#clickinit').click(function() {
+                                var documentWidth = $(document).width();
+                                var documentHeight = $(document).height();
+
+                                var $over = $('<div>')
+                                    .attr({id:'clickinitOver'
+                                          })
+                                    .css({position:'absolute',
+                                          opacity:'0.6',
+                                          width:documentWidth,
+                                          height:documentHeight,
+                                          backgroundColor:'#FFFFFF',
+                                          zIndex: 100});
+                                $('body').prepend($over);
+
                                 if (confirm('Clickinit ?')) {
+                                    alert('Wait one moment');
                                     $.get(clickinitInitUrl,
                                           null,
                                           function (res) {
                                               alert(res);
+                                              $over.remove();
                                           }
                                          );
+                                } else {
+                                    $over.remove();
                                 }
                             });
   });
